181ceb3cd50b6ad4eeea6eb83e970007e1828055
Maria_Torres <mafetorres622@gmail.com>
2024-11-19 11:40:23 -0500

    ya no se que p oner aqui3

--- src/components/Card/Cardpp.jsx
+++ src/components/Card/Cardpp.jsx
@@ -3,9 +3,24 @@ import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Cardpp = ({ title = 'Título no disponible', imageUrl = 'https://via.placeholder.com/300', description = 'Descripción no disponible', onDelete }) => {
+const Cardpp = ({ 
+  title = 'Título no disponible', 
+  imageUrl = 'https://via.placeholder.com/300', 
+  description = 'Descripción no disponible', 
+  onDelete 
+}) => {
   return (
-    <Card sx={{ maxWidth: 400, border: '1px solid #ccc', borderRadius: '8px', margin: '10px' }}>
+    <Card 
+      sx={{ 
+        maxWidth: 400, 
+        border: '1px solid #ccc', 
+        borderRadius: '8px', 
+        margin: '10px', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        justifyContent: 'space-between' 
+      }}
+    >
       <CardMedia
         component="img"
         height="140"
@@ -16,11 +31,11 @@ const Cardpp = ({ title = 'Título no disponible', imageUrl = 'https://via.place
         <Typography variant="h6" component="div">
           {title} 
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" sx={{ marginBottom: '10px' }}>
           {description} 
         </Typography>
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-          <button
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <button id="deleteBit"
             onClick={onDelete}
             style={{
               backgroundColor: 'transparent',
